92907f6f3d497279c05627f3e569467fd25e8b8b
Shaowei <Shaowei.Zhang@Outlook.com>
2019-05-05 16:18:31 +1000

    Passing Data to Recipe

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {RecipeModel} from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
+  @Input() recipe: RecipeModel;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
